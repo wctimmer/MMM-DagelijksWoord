271e396b7a9fff7f41d76869f26dde2a25a18028
Wim Timmer <wc.timmer@gmail.com>
2023-05-21 15:25:41 +0200

    Added and fixed lint for js and CSS

--- MMM-DagelijksWoord.js
+++ MMM-DagelijksWoord.js
@@ -7,76 +7,83 @@
  */
 
 Module.register("MMM-DagelijksWoord", {
-    // Default module config.
-    result: [],
-    defaults: {
-        translation: 'nbg21'
-    },
+  // Default module config.
+  result: [],
+  defaults: {
+    translation: "nbg21"
+  },
 
-    start: function() {
-        Log.info("Starting module: " + this.name);
-        var self = this;
-        
-        // empty text on start
-        this.verse = "";
-        this.source = "";
-        this.donationText = "";
+  start: function () {
+    Log.info(`Starting module: ${this.name}`);
+    var self = this;
 
-        //Do this once first
-        self.sendSocketNotification('START', this.config);
+    // empty text on start
+    this.verse = "";
+    this.source = "";
+    this.donationText = "";
 
-        //Then every 4 hour (change only once per day)
-        setInterval(function() {
-                self.sendSocketNotification('START', this.config);
-        }, 3600000*4); //perform every 4 hour (3600000 milliseconds in a hour)
-    },
+    //Do this once first
+    self.sendSocketNotification("START", this.config);
 
-    getStyles: function () {
-        return ["MMM-DagelijksWoord.css"];
-    },
+    //Then every 4 hour (change only once per day)
+    setInterval(function () {
+      self.sendSocketNotification("START", this.config);
+    }, 3600000 * 4); //perform every 4 hour (3600000 milliseconds in a hour)
+  },
 
-    // Override dom generator.
-    getDom: function() {
-        Log.log("Updating MMM-DagelijksWoord DOM.");
+  getStyles: function () {
+    return ["MMM-DagelijksWoord.css"];
+  },
 
-        var wrapper = document.createElement("div");
-        var wrapperVerse = document.createElement("div");
-		var labelSource = document.createElement("label");
-        
-        wrapperVerse.innerHTML = this.verse;
-        wrapperVerse.classList.add(this.name+"-verse");
-        labelSource.innerHTML = this.source;
-		labelSource.classList.add(this.name+"-source");	
-		
-        wrapper.appendChild(labelSource);
-        wrapper.appendChild(wrapperVerse);
-        
-        //show donation text on donation request day
-        if (this.donationDay()) { // only on donation request day (monday after christmas)
-            var wrapperDonation = document.createElement("div");
-            wrapperDonation.innerHTML = this.donationText;
-		    wrapperDonation.classList.add(this.name+"-donation");
-            wrapper.appendChild(wrapperDonation);
-            }
-        return wrapper;
-        },
+  // Override dom generator.
+  getDom: function () {
+    Log.log("Updating MMM-DagelijksWoord DOM.");
 
-    socketNotificationReceived: function(notification, payload) {
-        Log.log("socket received from Node Helper");
-        if(notification == "DAGELIJKS_WOORD_RESULT"){
-            var json = payload;
-            Log.log(payload);
-            this.verse = json.verse;
-            this.source = json.source;
-            this.donationText = json.donationText
-            this.updateDom();
-        }
-    },
-    
-    donationDay: function() {
-        const date = new Date();
-        if (date.getMonth() == 11 && date.getDate() == 27 && date.getDay != 0) return true; //day after chrismass and not on a sunday.
-        else if (date.getMonth() == 11 && date.getDate() == 28 && date.getDay == 1) return true; //day after chrismass when second chrismass day is a sunday.
-        else return false;
+    var wrapper = document.createElement("div");
+    var wrapperVerse = document.createElement("div");
+    var labelSource = document.createElement("label");
+
+    wrapperVerse.innerHTML = this.verse;
+    wrapperVerse.classList.add("dagelijkswoord-verse");
+    labelSource.innerHTML = this.source;
+    labelSource.classList.add("dagelijkswoord-source");
+
+    wrapper.appendChild(labelSource);
+    wrapper.appendChild(wrapperVerse);
+
+    //show donation text on donation request day
+    if (this.donationDay()) {
+      // only on donation request day (monday after christmas)
+      var wrapperDonation = document.createElement("div");
+      wrapperDonation.innerHTML = this.donationText;
+      wrapperDonation.classList.add("dagelijkswoord-donation");
+      wrapper.appendChild(wrapperDonation);
     }
+    return wrapper;
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    Log.log("socket received from Node Helper");
+    if (notification === "DAGELIJKS_WOORD_RESULT") {
+      var json = payload;
+      Log.log(payload);
+      this.verse = json.verse;
+      this.source = json.source;
+      this.donationText = json.donationText;
+      this.updateDom();
+    }
+  },
+
+  donationDay: function () {
+    const date = new Date();
+    if (date.getMonth() === 11 && date.getDate() === 27 && date.getDay !== 0)
+      return true; //day after chrismass and not on a sunday.
+    else if (
+      date.getMonth() === 11 &&
+      date.getDate() === 28 &&
+      date.getDay === 1
+    )
+      return true; //day after chrismass when second chrismass day is a sunday.
+    else return false;
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -6,66 +6,66 @@
  * MIT Licensed.
  */
 
+const https = require("https");
 var NodeHelper = require("node_helper");
-const https = require('https');
 
 module.exports = NodeHelper.create({
-	// Subclass start method.
-	start: function() {
-        console.log("Started node_helper.js for MMM-DagelijksWoord.");
-        
-	},
+  // Subclass start method.
+  start: function () {
+    console.log("Started node_helper.js for MMM-DagelijksWoord.");
+  },
 
-	socketNotificationReceived: function(notification, config) {
-		var self = this;
-        var options = {
-           host: 'feed.dagelijkswoord.nl',
-           port: 443,
-           path: '/api/json/1.0/',
-           // authentication headers
-           headers: {
-              'Authorization': 'Basic ' + new Buffer.from(config.username + ':' + config.password).toString('base64')
-           }   
-        };
-        
-        https.get(options, function(resp){
+  socketNotificationReceived: function (notification, config) {
+    var self = this;
+    var options = {
+      host: "feed.dagelijkswoord.nl",
+      port: 443,
+      path: "/api/json/1.0/",
+      // authentication headers
+      headers: {
+        Authorization: `Basic ${new Buffer.from(
+          `${config.username}:${config.password}`
+        ).toString("base64")}`
+      }
+    };
 
-            let body = '';
+    https
+      .get(options, function (resp) {
+        let body = "";
 
-            // A chunk of data has been received.
-            resp.on('data', (chunk) => {
-                body += chunk;
-            });
+        // A chunk of data has been received.
+        resp.on("data", (chunk) => {
+          body += chunk;
+        });
 
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                var data = JSON.parse(body);
-                var result = {}; // create empty result
-                
-                // select right translation                               
-                var verses = data.data[0].text;
-                result.verse = null;
-                for(key in verses)
-                {
-                    if (key === config.translation.toLowerCase()) {
-                         result.verse = verses[key];
-                    }
-                }
-                if (result.verse === null){
-                    console.error("MMM-DagelijksWoord: Translation not correct configured!");
-                }
-                    
-                result.source = data.data[0].source;
-                result.donationText = data.in_app_use.app_text;
-                console.log("MMM-DagelijksWoord: "+ result.source + ': ' + result.verse);
-                
-                
-				self.sendSocketNotification('DAGELIJKS_WOORD_RESULT', result);
-            });
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          var data = JSON.parse(body);
+          var result = {}; // create empty result
 
-       }).on("error", (err) => {
-           console.log("Error: " + err.message);
-       });
-  
-	},
+          // select right translation
+          var verses = data.data[0].text;
+          result.verse = null;
+          for (var key in verses) {
+            if (key === config.translation.toLowerCase()) {
+              result.verse = verses[key];
+            }
+          }
+          if (result.verse === null) {
+            console.error(
+              "MMM-DagelijksWoord: Translation not correct configured!"
+            );
+          }
+
+          result.source = data.data[0].source;
+          result.donationText = data.in_app_use.app_text;
+          console.log(`MMM-DagelijksWoord: ${result.source}: ${result.verse}`);
+
+          self.sendSocketNotification("DAGELIJKS_WOORD_RESULT", result);
+        });
+      })
+      .on("error", (err) => {
+        console.log(`Error: ${err.message}`);
+      });
+  }
 });
